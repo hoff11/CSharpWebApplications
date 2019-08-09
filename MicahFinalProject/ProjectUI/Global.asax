64579321d73dd737500ff2c2478c951725122bdd
Micah Hoffmann <micah.j.hoffmann@boeing.com>
2019-08-09 11:14:00 -0700

    Add new project: Micah Final Project
    MVC ui
    SQL project
    data library
    
    add student model > sign up controller > sing up view

---
+++ MicahFinalProject/ProjectUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectUI.MvcApplication" Language="C#" %>
---
+++ MicahFinalProject/ProjectUI/Models/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectUI.Models
+{
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public string StudentEmailAddress { get; set; }
+        public string StudentConfirmEmailAddress { get; set; }
+        public string StudentPassword { get; set; }
+        public string StudentConfirmPassword { get; set; }
+    }
+}
