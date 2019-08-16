73564d8c6eb4e172d109c73bf30f81a0b389c591
Micah Hoffmann <micah.j.hoffmann@boeing.com>
2019-08-16 14:21:26 -0700

    sp_New_AccountRole

--- MicahFinalProject/MicahHoffmannFinalDB.sql
+++ MicahFinalProject/MicahHoffmannFinalDB.sql
@@ -313,3 +313,52 @@ BEGIN
 		@Status 
 	)
 END
+
+DROP PROCEDURE IF EXISTS spNew_AccountRole;
+GO
+
+CREATE PROCEDURE spNew_AccountRole
+	@AccountID nvarchar(50),
+	@RoleName nvarchar(50)
+AS
+BEGIN
+	DECLARE @AccountRoleID nvarchar(50)
+	DECLARE @RoleID nvarchar(50)
+	
+	SELECT @RoleID = Id
+	FROM Role
+	WHERE Name = @RoleName
+	
+	IF @RoleID IS NULL
+		BEGIN
+			INSERT INTO Role(
+				Id,
+				Name
+			)VALUES(
+				NEWID(),
+				@RoleName
+			)
+			
+			SELECT @RoleID = Id
+			FROM Role
+			WHERE Name = @RoleName
+		END
+	
+	SELECT @AccountRoleID = AccountRoleID
+	FROM AccountRole
+	WHERE AccountID = @AccountID AND RoleID = @RoleID
+	
+	IF @AccountRoleID IS NULL
+		BEGIN
+			INSERT INTO AccountRole(
+				AccountRoleID,
+				AccountID,
+				RoleID
+			)VALUES(
+				NEWID(),
+				@AccountID,
+				@RoleID
+			)
+		END 
+
+END
